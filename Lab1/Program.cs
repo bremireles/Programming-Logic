8fde069f4ce33f7f669b7213b68fdaf60b897ed1
bremireles <S01229323@acad.tri-c.edu>
2026-07-06 19:31:25 +0000

    Lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hey Bre! Welcome to IT-1050");
+    }
+}
